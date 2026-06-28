0d2cf9d25d1cdb0c45a7121411a8473af20428ae
Shyam Kunda-Pibit <shyam.kunda@pibit.ai>
2026-06-28 23:56:07 +0530

    added negative test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -17,3 +17,14 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('negative test forget started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.waitForTimeout(10000);
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).not.toBeVisible();
+});
