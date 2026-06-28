6bfa7fb84218942483e45a2e212f7a150b1b18ae
Shyam Kunda-Pibit <shyam.kunda@pibit.ai>
2026-06-28 21:07:52 +0530

    updated spec

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -12,6 +12,7 @@ test('get started link', async ({ page }) => {
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
+  await page.waitForTimeout(10000);
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
